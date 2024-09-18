8ba2840728c5390aca7b92bf236d721d44103de0
Dee <dom.aves@gmail.com>
2024-09-18 19:27:58 +0100

    input_print .py

---
+++ .py
@@ -0,0 +1,14 @@
+# 100 days of code
+print("what is your name?")
+w = input()
+print("Whats your favorie food?")
+x = input()
+print("Whats your favorie music?")
+y = input()
+print("Where do you live?")
+z = input()
+
+print('Hello, ' + w)
+print( x + ' .I like that as well, ')
+print( y + ' .Nice!')
+print( z + ' .Nice place to live,')
